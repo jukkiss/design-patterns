507cf502bb803ed1221955bdf0bb4655557b16e2
Jukka <savela.j@gmail.com>
2024-10-08 19:04:08 +0300

    "handlers"

--- ChainOfResponsibility/CustomerFeedbackHandler/CustomerFeedbackHandler/src/main/java/Main.java
+++ ChainOfResponsibility/CustomerFeedbackHandler/CustomerFeedbackHandler/src/main/java/Main.java
@@ -4,15 +4,15 @@ public class Main {
         Handler contactHandler = new ContactRequestHandler();
         Handler suggestionHandler = new DevelopmentSuggestionHandler();
         Handler feedbackHandler = new GeneralFeedbackHandler();
-
+        // Handler order below
         compensationHandler.setNextHandler(contactHandler);
         contactHandler.setNextHandler(suggestionHandler);
         suggestionHandler.setNextHandler(feedbackHandler);
 
-        Message message1 = new Message(Message.MessageType.COMPENSATION_CLAIM, "I want a refund.", "customer1@example.com");
-        Message message2 = new Message(Message.MessageType.CONTACT_REQUEST, "Please call me back.", "customer2@example.com");
-        Message message3 = new Message(Message.MessageType.DEVELOPMENT_SUGGESTION, "I have a feature idea.", "customer3@example.com");
-        Message message4 = new Message(Message.MessageType.GENERAL_FEEDBACK, "Great service!", "customer4@example.com");
+        Message message1 = new Message(Message.MessageType.COMPENSATION_CLAIM, "I want my money back!", "customer1@email.com");
+        Message message2 = new Message(Message.MessageType.CONTACT_REQUEST, "Please call me back.", "customer2@mail.com");
+        Message message3 = new Message(Message.MessageType.DEVELOPMENT_SUGGESTION, "I have a feature idea.", "customer3@mail.com");
+        Message message4 = new Message(Message.MessageType.GENERAL_FEEDBACK, "Great service!", "customer4@mail.com");
 
         compensationHandler.handle(message1);
         compensationHandler.handle(message2);
